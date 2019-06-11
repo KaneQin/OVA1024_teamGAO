c5b9888cad422a97b7daa926e77e4b0a4b3b9c38
NYJ9527 <1010528925@qq.com>
2019-06-11 15:19:50 +0800

    123

---
+++ urls.py
@@ -0,0 +1,26 @@
+HOST = 'http://47.92.220.226'
+LOGIN_PAGE_URL = HOST + '/bbs/index.php/user/login'
+'''
+首页
+'''
+HOME_PAGE_URL = HOST + '/bbs/'
+'''
+小组
+'''
+GROUP_PAGE_URL = HOST + '/bbs/index.php/group'
+'''
+文章
+'''
+ARTICLE_PAGE_URL = HOST + '/bbs/index.php/article'
+'''
+唠叨
+'''
+TO_NAG_PAGE_URL = HOST + '/bbs/index.php/weibo'
+'''
+用户
+'''
+USER_PAGE_URL = HOST + '/bbs/index.php/user'
+'''
+搜索
+'''
+SEARCH_PAGE_URL = HOST + '/bbs/index.php/search'
