5a465c926b00b7058cb641f7660b124fe2413232
KaneQin <352093042@qq.com>
2019-06-11 15:46:00 +0800

    test page

--- coffee/utils/urls.py
+++ coffee/utils/urls.py
@@ -1,3 +1,26 @@
-# -*- encoding:utf-8 -*-
-HOST='http://47.92.220.226/bbs/'
-LOGIN_PAGE_URL = HOST + 'index.php/user/login'
+HOST = 'http://47.92.220.226'
+LOGIN_PAGE_URL = HOST + '/bbs/index.php/user/login'
+'''
+首页
+'''
+HOME_PAGE_URL = HOST + '/bbs/'
+'''
+小组
+'''
+GROUP_PAGE_URL = HOST + '/bbs/index.php/group'
+'''
+文章
+'''
+ARTICLE_PAGE_URL = HOST + '/bbs/index.php/article'
+'''
+唠叨
+'''
+TO_NAG_PAGE_URL = HOST + '/bbs/index.php/weibo'
+'''
+用户
+'''
+USER_PAGE_URL = HOST + '/bbs/index.php/user'
+'''
+搜索
+'''
+SEARCH_PAGE_URL = HOST + '/bbs/index.php/search'
