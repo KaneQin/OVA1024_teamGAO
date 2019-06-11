3dcc937a03ba4349e5dda58643427361a806ac29
GaoLinCong79 <1690344964@qq.com>
2019-06-11 20:48:19 +0800

    创建小组03

--- coffee/ova1024/gruoppage/group.py
+++ coffee/ova1024/gruoppage/group.py
@@ -7,9 +7,11 @@ class CreateGroup(CoffeeDriver):
     keyword = u"发布帖子"
     def __init__(self):
         self.driver=driver
+    @property
     def label_group(self):
         element = self.driver.find_element_by_xpath("/html/body/div[1]/div/a[2]")
         return element
+    @property
     def label_create_group(self):
         element=self.driver.find_element_by_xpath("/html/body/div[3]/nav/a")
         return element
@@ -29,18 +31,19 @@ class CreateGroup(CoffeeDriver):
     def grouptag(self):
         element=self.driver.find_element_by_name("tag")
         return element
+    @property
     def label_creategroup(self):
         element=self.driver.find_element_by_xpath("//*[@type='submit']")
         return element
     def create(self,groupname,groupdesc,grouptag):
-        self.label_create_group().click()
+        self.label_create_group.click()
         self.Debug("向小组名称控件中输入名称：%s" % groupname)
         self.groupname.send_keys(groupname)
         self.Debug("向小组介绍控件中输入描述：%s" % groupdesc)
         self.groupdesc.send_keys(groupdesc)
         self.Debug("向小组标签控件中输入标签：%s" % grouptag)
         self.grouptag.send_keys(grouptag)
-        self.label_creategroup().click()
+        self.label_creategroup.click()
 
 
 
