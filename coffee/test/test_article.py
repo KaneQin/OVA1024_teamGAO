5dbce2bde3df9cfe1cc016b82925f1feab4be03b
Administrator <615800841@qq.com>
2019-06-11 18:56:43 +0800

    文章

---
+++ coffee/test/test_article.py
@@ -0,0 +1,24 @@
+# encoding:utf-8
+
+import unittest
+from coffee.ova1024.articlepage.Article import Article
+from coffee.ova1024.loginpage.login import login
+class MyTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        login().Sign_in("615800841@qq.com","huxu0315")
+
+
+    def test_something(self):
+
+       ext=Article().Publish()
+
+       #实际值
+       arts="http://47.92.220.226/bbs/index.php/article/index/page-2"
+
+
+       self.assertEqual(arts, arts)
+
+
+if __name__ == '__main__':
+    unittest.main()
